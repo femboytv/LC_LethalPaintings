d6df70b313c3acd9112c182f48d9119fb6396923
ElonGates <59313695+VilotStar@users.noreply.github.com>
2023-11-30 00:54:57 +0000

    init

---
+++ Plugin.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using BepInEx;
+using HarmonyLib;
+
+namespace LethalPaintings
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+            PosterFolders = Directory.GetDirectories(Paths.PluginPath, PluginInfo.PLUGIN_NAME, SearchOption.AllDirectories).ToList();
+            
+            foreach (var file in PosterFolders)
+            {
+                if (Path.GetExtension(file) != ".old") { 
+                    PaintingFiles.Add(file);
+                }
+            }
+            
+            Patches.Init(Logger);
+
+            var harmony = new Harmony(PluginInfo.PLUGIN_GUID);
+            harmony.PatchAll(typeof(Patches));
+            
+            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+        }
+        
+        private static List<string> PosterFolders = new();
+        public static readonly List<string> PaintingFiles = new();
+        public static Random Rand = new();
+    }
+    
+}
